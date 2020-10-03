ce68554c3ed70f8aa7339b4c38c1a4ed5e1e2f7e
Shouvik-99 <72292341+Shouvik-99@users.noreply.github.com>
2020-10-03 19:46:22 +0530

    diff-method.py
    dictionary.py

--- distinct-strings.py
+++ distinct-strings.py
@@ -31,7 +31,50 @@ Sample Output
 """
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+import time
+from collections import defaultdict
 s = set()
+t1=time.time()
 for _ in range(0,int(input())):
     s.add(input())
 print(len(s))
+t2=time.time()
+print(t2-t1)
+
+d=defaultdict(int)
+t1=time.time()
+for i in range(0,int(input())):
+    d[i]+=1
+print(len(d))
+t2=time.time()
+print(t2-t1)
+
+'''
+    A different approach using dictionary as a set
+        tc==>O(n)
+    For both method
+    Here the O/P with running time with both method.
+    
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+5
+2.095736503601074
+
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+7
+2.3257291316986084
+
+'''
