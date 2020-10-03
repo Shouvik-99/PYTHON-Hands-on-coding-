e993a69a851020a467acb10093feb884a4f79650
Shouvik-99 <72292341+Shouvik-99@users.noreply.github.com>
2020-10-03 14:50:16 +0530

    rot_update.py
    my_update.py

--- Rotating-a-list.py
+++ Rotating-a-list.py
@@ -25,18 +25,10 @@ Sample Output
 5 1 2 3 4
 
 '''
-
-def rotLeft(a, num):
-    length = len(a)
-    b = []
-    if(num > length):
-        num = num % length
-    b = a[num:length] + a[0:num]
-    return b
-
-
-
+import time
+t1=time.time()
 if __name__ == '__main__':
+
     nd = input().split()
 
     n = int(nd[0])
@@ -44,7 +36,39 @@ if __name__ == '__main__':
     d = int(nd[1])
 
     a = list(map(int, input().rstrip().split()))
+def rotLeft(a, num):
+    length = len(a)
+    b = []
+    if(num > length):
+        num = num % length
+    b = a[num:length] + a[0:num]
+    return b
+result = rotLeft(a, d)
+print(' '.join(map(str, result)))
+t2=time.time()
+print(t2-t1)
 
-    result = rotLeft(a, d)
 
-    print(' '.join(map(str, result)))
+t3=time.time()
+n,s=map(int,input().split())       # Using input format,in short manner
+arr=list(map(int,input().split())) # better understandable also
+mod_arr=[0]*n
+for i in range(n):
+    ind=(i-s)%n
+    mod_arr[ind]=arr[i]
+print(*mod_arr)
+t4=time.time()
+print(t4-t3)
+
+'''
+O/P==>
+5 4
+1 2 3 4 5
+5 1 2 3 4
+6.698445796966553
+5 1 2 3 4
+6.203363656997681
+
+Time complexity same O(n),
+Using pyhton bidirectional indexing.
+'''
